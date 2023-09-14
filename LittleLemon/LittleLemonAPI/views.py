694284308a240d2b9e14af850a61ad4f072731ba
aniebietafia <aniebietafia87@gmail.com>
2023-09-14 21:09:26 +0100

    adding function for crud on menu items

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -6,6 +6,13 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def menu_items(request):
-    return Response('Menu Items', status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        return Response('Menu Items', status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        return Response('Menu Items', status=status.HTTP_201_CREATED)
+    if request.method == 'PUT':
+        return Response('Menu Items', status=status.HTTP_200_OK)
+    if request.method == 'DELETE':
+        return Response('Menu Items', status=status.HTTP_204_NO_CONTENT)
