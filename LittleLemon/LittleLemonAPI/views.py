d729f2b8911da88845a756ee10c0b82d3d2da7d5
aniebietafia <aniebietafia87@gmail.com>
2023-09-14 21:00:37 +0100

    initializing the app

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def menu_items(request):
+    return Response('Menu Items', status=status.HTTP_200_OK)
