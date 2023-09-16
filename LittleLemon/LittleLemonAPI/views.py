70dc92d4228bd534ddd92caedc7f366840a2d524
aniebietafia <aniebietafia87@gmail.com>
2023-09-16 21:01:56 +0100

    creating views

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,18 +1,16 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your views here.
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def menu_items(request):
-    if request.method == 'GET':
-        return Response('Fetch All Menu Items', status=status.HTTP_200_OK)
-    if request.method == 'POST':
-        return Response('Menu Item Posted Successfully', status=status.HTTP_201_CREATED)
-    if request.method == 'PUT':
-        return Response('Menu Item has been Updated', status=status.HTTP_200_OK)
-    if request.method == 'DELETE':
-        return Response('Menu Item has been Deleted', status=status.HTTP_204_NO_CONTENT)
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
