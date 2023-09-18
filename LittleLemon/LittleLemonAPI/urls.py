6322fe742e7fc3bbf1a67e62afad2a20440974e2
aniebietafia <aniebietafia87@gmail.com>
2023-09-18 20:45:52 +0100

    app urls

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-   path('menu-items/', views.MenuItemsView.as_view()),
-   path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+   path('menu-items/', views.menu_items),
+   path('menu-item/<int:pk>', views.menu_item),
+   path('api-auth-token/', obtain_auth_token)
 ]
