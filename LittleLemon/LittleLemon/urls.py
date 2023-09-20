9e57a294fa11d489f825b54575b9b31507394ebf
aniebietafia <aniebietafia87@gmail.com>
2023-09-20 22:53:57 +0100

    project level urls

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -14,6 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('LittleLemonDRF.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
+
+
+"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,3 +37,4 @@ urlpatterns = [
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
 ]
+"""
